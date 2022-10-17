eb089fb91a3fe79804689c5bee938fa0a09e6217
Benyamin Ginzburg <benyomin.94@gmail.com>
2022-10-18 00:07:21 +0300

    [WIP] ws tracking

--- israel_transport_api/siri/models.py
+++ israel_transport_api/siri/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from pydantic import BaseModel, Field
 
 from israel_transport_api.gtfs.models import Stop, Route
+from israel_transport_api.siri.siri_models import VehicleLocation
 
 
 class IncomingRoutesResponse(BaseModel):
@@ -13,7 +14,20 @@ class IncomingRoutesResponse(BaseModel):
 
 class IncomingRoute(BaseModel):
     eta: int
+    plate_number: str
     route: Route
+
+
+class VehicleLocationResponse(BaseModel):
+    latitude: float
+    longitude: float
+
+    @classmethod
+    def from_siri_model(cls, siri_model: VehicleLocation):
+        return cls(
+            latitude=siri_model.latitude,
+            longitude=siri_model.longitude
+        )
 
 
 IncomingRoutesResponse.update_forward_refs()
--- israel_transport_api/siri/router.py
+++ israel_transport_api/siri/router.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter, Path, Query, Request
+from asyncio import sleep
 
-from israel_transport_api.siri.client import get_incoming_routes
-from israel_transport_api.siri.models import IncomingRoutesResponse
+from fastapi import APIRouter, Path, Query, Request, WebSocket, WebSocketDisconnect
+
+from israel_transport_api.siri.client import get_incoming_routes, get_vehicle_location
+from israel_transport_api.siri.models import IncomingRoutesResponse, VehicleLocationResponse
+
+
+WS_UPDATE_INTERVAL = 3
 
 siri_router = APIRouter(prefix='/siri', tags=['Siri'])
 
@@ -13,3 +18,20 @@ async def get_routes_for_stop(
         monitoring_interval: int = Query(30, description='Monitoring interval in minutes')
 ) -> IncomingRoutesResponse:
     return await get_incoming_routes(request.app.state.conn, stop_code, monitoring_interval)
+
+
+@siri_router.websocket('/track_vehicle/{stop_code}/{vehicle_plate_number}')
+async def track_vehicle(
+        ws: WebSocket,
+        stop_code: int = Path(..., description='Stop code for tracking'),
+        vehicle_plate_number: str = Path(..., description='Vehicle plate number')
+):
+    await ws.accept()
+    try:
+        while True:
+            resp = await get_vehicle_location(vehicle_plate_number, stop_code)
+            resp and await ws.send_json(resp.dict())
+            await sleep(WS_UPDATE_INTERVAL)
+    except WebSocketDisconnect:
+        pass
+
